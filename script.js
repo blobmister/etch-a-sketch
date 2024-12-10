245eb359885224babc64b6930be456de3b01aee6
Krish Gupta <179193922+blobmister@users.noreply.github.com>
2024-12-10 16:40:25 +1100

    Added grid toggle button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function generateGridDefault(n = 16) {
+function generateGridDefault(n = 16, gridOn) {
     let container = document.querySelector(".gridContainer");
     container.innerHTML = '';
     
@@ -10,6 +10,11 @@ function generateGridDefault(n = 16) {
             box.classList.add("box");
 
             box.addEventListener('mouseover', () => hover(box));
+            if (gridOn) {
+                box.style.border = '1px solid black';
+            } else {
+                box.style.border = '0px solid black';
+            }
 
             row.appendChild(box);
         }
@@ -17,7 +22,7 @@ function generateGridDefault(n = 16) {
     }
 }
 
-function generateGridColour(n = 16) {
+function generateGridColour(n = 16, ridOn) {
     let container = document.querySelector(".gridContainer");
     container.innerHTML = '';
     
@@ -30,13 +35,19 @@ function generateGridColour(n = 16) {
 
             box.addEventListener('mouseover', () => hoverColour(box));
 
+            if (gridOn) {
+                box.style.border = '1px solid black';
+            } else {
+                box.style.border = '0px solid black';
+            }
+
             row.appendChild(box);
         }
         container.appendChild(row);
     }
 }
 
-function generateGridOpacity(n = 16) {
+function generateGridOpacity(n = 16, gridOn) {
     let container = document.querySelector(".gridContainer");
     container.innerHTML = '';
     
@@ -49,6 +60,12 @@ function generateGridOpacity(n = 16) {
 
             box.colour = 'rgba(0, 0, 0, 0)';
             box.addEventListener('mouseover', () => hoverOpacity(box));
+
+            if (gridOn) {
+                box.style.border = '1px solid black';
+            } else {
+                box.style.border = '0px solid black';
+            }
 
             row.appendChild(box);
         }
@@ -83,39 +100,59 @@ function hoverOpacity(box) {
     }
 }
 
-function changeMode(mode, n) {
+function changeMode(mode, n, gridOn) {
     if (mode == 'default') {
-        generateGridDefault(n);
+        generateGridDefault(n, gridOn);
     } else if (mode == 'rgb') {
-        generateGridColour(n);
+        generateGridColour(n, gridOn);
     } else if (mode == 'opacity') {
-        generateGridOpacity(n);
+        generateGridOpacity(n, gridOn);
+    }
+}
+
+function toggleGrid() {
+    let box = document.querySelectorAll(".box");
+
+    for (let i = 0; i < box.length; i++) {
+        if (box[i].style.border == '1px solid black') {
+            box[i].style.border = '0px solid black';
+        } else if (box[i].style.border == '0px solid black') {
+            box[i].style.border = '1px solid black';
+        }
     }
 }
 
 
 let n = 16;
 let currentMode = 'default';
-generateGridDefault();
+let gridOn = true;
+generateGridDefault(16, gridOn);
 let defaultButton = document.querySelector("#default");
 let rgbButton = document.querySelector("#rgb");
 let opacityButton = document.querySelector("#opacity");
 let input = document.querySelector("input");
+let gridToggle = document.querySelector('#toggle-grid')
+
+
+gridToggle.addEventListener('click', () => {
+    toggleGrid();
+    gridOn = !gridOn;
+})
 
 defaultButton.addEventListener('click', () => {
     currentMode = 'default';
-    changeMode('default', n);
-});
+    changeMode('default', n, gridOn);
+})
+
 rgbButton.addEventListener('click', () => {
     currentMode = 'rgb';
-    changeMode('rgb', n);
+    changeMode('rgb', n, gridOn);
 })
 
 opacityButton.addEventListener('click', () => {
     currentMode = 'opacity'
-    changeMode('opacity', n);
+    changeMode('opacity', n, gridOn);
 })
-
 
 input.addEventListener('keydown', function(e) {
     if(e.key === "Enter") {
